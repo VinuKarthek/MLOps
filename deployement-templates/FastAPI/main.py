e3b1611d0153e6a29e7a21c225697683df75ff83
rajamanivinu <VinuKarthek.Rajamani@infineon.com>
2024-04-06 18:45:04 +0800

    Server ML Model using FastAPI & Containerize it

---
+++ deployement-templates/FastAPI/main.py
@@ -0,0 +1,59 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi import APIRouter, FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from fastapi import FastAPI, File, HTTPException, UploadFile
+
+from typing import Any
+from pydantic import BaseModel, Field, ValidationError
+import pickle
+import pandas as pd
+ 
+class Model (BaseModel):
+    fixed_acidity: float  = Field(alias='fixed acidity')
+    volatile_acidity: float  = Field(alias='volatile acidity')
+    citric_acid : float  = Field(alias='citric acid')
+    residual_sugar: float = Field(alias='residual sugar')
+    chlorides: float
+    free_sulfur_dioxide: int = Field(alias='free sulfur dioxide')
+    total_sulfur_dioxide: int = Field(alias='total sulfur dioxide')
+    density: float
+    pH: float
+    sulphates: float 
+    alcohol: float
+
+def json_verify(data_in) -> list[Any]:
+    try:
+        Model(**data_in)
+        return(True)
+    except ValidationError as e:
+        return(e.json())
+            
+def load_model(path:str) -> object:
+    return pickle.load(open(path, 'rb'))
+
+MODEL_REV = 100
+MODEL_NAME = 'wine-quality'
+model = load_model(f'models/{MODEL_NAME}_v{MODEL_REV}.pkl')
+
+app = FastAPI()
+
+
+@app.post(f"/predict/{MODEL_NAME}", description=f'Retuns Prediction & Probability (for Classification) using Model - "{MODEL_NAME}_v{MODEL_REV}"')
+async def inference(data_in: Model):
+
+    data = list(data_in.dict().values())
+    prediction = model.predict([data])
+    try:
+        probability = model.predict_proba([data])
+    except:
+        probability = 'NA'
+    return {
+        'prediction': prediction[0],
+        'probability': probability
+    }
+
+if __name__=="__main__":
+    uvicorn.run(app, host='127.0.0.1', port=8000)
