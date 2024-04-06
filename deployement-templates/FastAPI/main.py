bcbc3e12a855954c45e4d923257b6aee680a72bd
rajamanivinu <VinuKarthek.Rajamani@infineon.com>
2024-04-06 18:48:52 +0800

    Added Test Page & Batch Processing

--- deployement-templates/FastAPI/main.py
+++ deployement-templates/FastAPI/main.py
@@ -51,6 +51,11 @@ async def favicon():
     #Favicon for your model page
     return FileResponse('landing/src/images/favicon.png')
 
+@app.post('/test', description= "Returns the message you send")
+async def hello_world(text_msg:str):
+    print('Your Message -> ',text_msg)
+    return {'message':text_msg}
+
 @app.post(f"/predict/{MODEL_NAME}", description=f'Retuns Prediction & Probability (for Classification) using Model - "{MODEL_NAME}_v{MODEL_REV}"')
 async def inference(data_in: Model):
 
@@ -63,6 +68,17 @@ async def inference(data_in: Model):
     return {
         'prediction': prediction[0],
         'probability': probability
+    }
+
+@app.post(f'/predict-batch/{MODEL_NAME}', description=f'Retuns List of Prediction using Model - "{MODEL_NAME}_v{MODEL_REV}"')
+async def batch_inference(csv_file: UploadFile = File(...)):
+    print('Processing ', csv_file.filename)
+    df = pd.read_csv(csv_file.file)                         #Read the batch file
+    df.drop(['quality'],axis =1, inplace = True)   
+    prediction = model.predict(df)   
+    print(prediction)                   
+    return {
+        'prediction': prediction,
     }
 
 if __name__=="__main__":
